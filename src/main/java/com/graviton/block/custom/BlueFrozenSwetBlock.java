8afd2855d1e381b98636693ba0e5a5e020b72317
Zippy <zippybrn@gmail.com>
2023-06-19 11:36:24 -0400

    switched swet block properties
    felt that making swet blocks like slime makes more sense

--- src/main/java/com/graviton/block/custom/BlueFrozenSwetBlock.java
+++ src/main/java/com/graviton/block/custom/BlueFrozenSwetBlock.java
@@ -1,11 +1,14 @@
 package com.graviton.block.custom;
 
 import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.SlimeBlock;
+import net.minecraft.world.level.block.HoneyBlock;
+import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.Material;
 
-public class BlueFrozenSwetBlock extends SlimeBlock {
+public class BlueFrozenSwetBlock extends HoneyBlock {
     public BlueFrozenSwetBlock() {
-        super(BlockBehaviour.Properties.copy(Blocks.HONEY_BLOCK));
+        super(BlockBehaviour.Properties.copy(Blocks.HONEY_BLOCK).sound(SoundType.SLIME_BLOCK));
     }
 }
+
--- src/main/java/com/graviton/block/custom/BlueSwetBlock.java
+++ src/main/java/com/graviton/block/custom/BlueSwetBlock.java
@@ -2,11 +2,10 @@ package com.graviton.block.custom;
 
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.HoneyBlock;
-import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.SlimeBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.Material;
 
-public class BlueSwetBlock extends HoneyBlock {
+public class BlueSwetBlock extends SlimeBlock {
     public BlueSwetBlock() {
         super(BlockBehaviour.Properties.copy(Blocks.SLIME_BLOCK));
     }
