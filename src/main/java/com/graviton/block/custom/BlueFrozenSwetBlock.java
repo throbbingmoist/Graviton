fc2bf5c9ea5b7e1acc0c4bfe11837c9fcd92f94b
unknown <thelumpsemail@gmail.com>
2023-07-09 17:46:09 -0400

    Added slime block and honey block functionality to aether alts.

--- src/main/java/com/graviton/block/custom/BlueFrozenSwetBlock.java
+++ src/main/java/com/graviton/block/custom/BlueFrozenSwetBlock.java
@@ -1,13 +1,28 @@
 package com.graviton.block.custom;
 
+import com.graviton.block.GravitonBlocks;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.HoneyBlock;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.BlockState;
+
+import java.util.function.BiPredicate;
 
 public class BlueFrozenSwetBlock extends HoneyBlock {
+
+    private final BiPredicate<BlockState, BlockState> stickyPredicate = (state, other) -> other.getBlock() != state.getBlock();
     public BlueFrozenSwetBlock() {
-        super(BlockBehaviour.Properties.copy(Blocks.HONEY_BLOCK).sound(SoundType.SLIME_BLOCK));
+        super(BlockBehaviour.Properties.copy(Blocks.SLIME_BLOCK));
+    }
+
+    @Override
+    public boolean isStickyBlock(BlockState state) {
+        return true;
+    }
+    @Override
+    public boolean canStickTo(BlockState state, BlockState other) {
+        return stickyPredicate.test(GravitonBlocks.BLUE_SWET_BLOCK.get().defaultBlockState(), other) && stickyPredicate.test(Blocks.SLIME_BLOCK.defaultBlockState(), other);
     }
 }
 
--- src/main/java/com/graviton/block/custom/BlueSwetBlock.java
+++ src/main/java/com/graviton/block/custom/BlueSwetBlock.java
@@ -1,11 +1,28 @@
 package com.graviton.block.custom;
 
 import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.SlimeBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.SlimeBlock;
+
+import java.util.function.BiPredicate;
+
+import net.minecraft.world.level.block.state.BlockBehaviour.Properties;
+import com.graviton.block.GravitonBlocks;
 
 public class BlueSwetBlock extends SlimeBlock {
+
+    private final BiPredicate<BlockState, BlockState> stickyPredicate = (state, other) -> other.getBlock() != state.getBlock();
     public BlueSwetBlock() {
         super(BlockBehaviour.Properties.copy(Blocks.SLIME_BLOCK));
+    }
+
+    @Override
+    public boolean isStickyBlock(BlockState state) {
+        return true;
+    }
+    @Override
+    public boolean canStickTo(BlockState state, BlockState other) {
+        return stickyPredicate.test(GravitonBlocks.BLUE_FROZEN_SWET_BLOCK.get().defaultBlockState(), other) && stickyPredicate.test(Blocks.HONEY_BLOCK.defaultBlockState(), other);
     }
 }
